a9504f8d389799aa869228bf996cdd7b8e16db5c
Darcy Kimball <asanchez@doglover.com>
2018-04-03 05:15:55 +0000

    Added filesystem type test macros

--- src/TSK/Internal/FileSystem.hs
+++ src/TSK/Internal/FileSystem.hs
@@ -8,9 +8,43 @@ import Foreign.C.Types
 
 import qualified Language.C.Inline as C
 
+import TSK.Internal.TH
+import TSK.Internal.Types
+
+
+C.context tskCtx
 
 C.include "tsk/libtsk.h"
 
 
-versionStr :: IO CString
-versionStr = [C.exp| const char* { tsk_version_get_str() } |]
+fsTypeIsNTFS, fsTypeIsFAT, fsTypeIsFFS, fsTypeIsEXT :: FSTypeEnum -> Bool
+fsTypeIsISO9660, fsTypeIsHFS, fsTypeIsSwap          :: FSTypeEnum -> Bool
+fsTypeIsYAFFS2, fsTypeIsRaw                          :: FSTypeEnum -> Bool
+
+
+fsTypeIsNTFS fsType =
+  [C.pure| int { TSK_FS_TYPE_ISNTFS($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsFAT fsType =
+  [C.pure| int { TSK_FS_TYPE_ISFAT($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsFFS fsType =
+  [C.pure| int { TSK_FS_TYPE_ISFFS($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsEXT fsType =
+  [C.pure| int { TSK_FS_TYPE_ISEXT($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsISO9660 fsType =
+  [C.pure| int { TSK_FS_TYPE_ISISO9660($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsHFS fsType =
+  [C.pure| int { TSK_FS_TYPE_ISHFS($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsSwap fsType =
+  [C.pure| int { TSK_FS_TYPE_ISSWAP($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsYAFFS2 fsType =
+  [C.pure| int { TSK_FS_TYPE_ISYAFFS2($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
+
+fsTypeIsRaw fsType =
+  [C.pure| int { TSK_FS_TYPE_ISRAW($(TSK_FS_TYPE_ENUM fsType)) } |] /= 0
